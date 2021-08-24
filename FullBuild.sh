4d558822b06d6f137858a85c01dcbc42178123f5
JonMcC <j.mccullough@ucl.ac.uk>
2021-08-24 15:18:49 +0100

    Add SNG to build script

--- FullBuild.sh
+++ FullBuild.sh
@@ -7,11 +7,15 @@
 MODULES(){
 
 #Module environment on ARCHER2
-module restore PrgEnv-gnu 
+#module restore PrgEnv-gnu 
+
+#Module environment on SuperMUC-NG, default compilers are fine
+module load cmake
 
 module list
 #Export compiler shortcuts as named on given machine
-export FC=ftn && export CC=cc && export CXX=CC
+export CC=mpiicc && export CXX=mpiicpc
+
 }
 
 ## HEMELB build
@@ -27,14 +31,28 @@ make -j  && echo "Done HemeLB Dependencies"
 cd ../..
 }
 
-
-SRCbuild(){
+SRCbuild_ARCHER2(){
 cd src
 rm -rf build
 mkdir build
 cd build
 
 cmake -DCMAKE_Fortran_COMPILER=${FC} -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DCTEMPLATE_LIBRARY=<PathToRepoOnARCHER2>/dep/install/lib/libctemplate.a -DHEMELB_USE_MPI_WIN=OFF ..
+
+
+make -j && echo "Done HemeLB Source"
+
+cd ../..
+}
+
+
+SRCbuild_SNG(){
+cd src
+rm -rf build_DEFAULT
+mkdir build_DEFAULT
+cd build_DEFAULT
+
+cmake -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DHEMELB_USE_GMYPLUS=OFF -DHEMELB_USE_MPI_WIN=ON -DHEMELB_USE_SSE3=ON -DHEMELB_USE_AVX2=OFF ..
 
 make -j && echo "Done HemeLB Source"
 
@@ -43,4 +61,4 @@ cd ../..
 
 MODULES
 DEPbuild
-SRCbuild
+SRCbuild_SNG
